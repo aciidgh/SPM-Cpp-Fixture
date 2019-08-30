e8d29de47f01bfdf61deaa02a251d9d5b18c8d19
Christian Treffs <ctreffs@gmail.com>
2019-08-30 09:42:27 +0200

    Update Package.swift to Swift version 5 - updates the swift-tools-version - updates targets & products

--- Package.swift
+++ Package.swift
@@ -1,9 +1,32 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "Objc",
-    targets: [Target(name: "cpp-exec", dependencies:["cpplib"]),
-              Target(name: "cwrapper", dependencies:["cpplib"]),
-              Target(name: "swift-exec", dependencies:["cwrapper"]),
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(name: "cpplib",
+                 targets: ["cpplib"]),
+        .library(name: "cwrapper",
+                 targets: ["cwrapper"]),
+        .executable(name: "cpp-exec",
+                    targets: ["cpp-exec"]),
+        .executable(name: "swift-exec",
+                    targets: ["swift-exec"])
+
+    ],
+    dependencies: [],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(name: "cpplib"),
+        .target(name: "cpp-exec",
+                dependencies: ["cpplib"]),
+        .target(name: "cwrapper",
+                dependencies: ["cpplib"]),
+        .target(name: "swift-exec",
+                dependencies: ["cwrapper"])
     ]
 )
